c405980baedbbb54fb3a17ebe07fd8b7824d6e30
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-02-09 14:15:06 -0300

    pattern adapter

--- src/main/java/adapter/Pagamento.java
+++ src/main/java/adapter/Pagamento.java
@@ -1,2 +1,30 @@
-package adapter;public class Pagamento {
+package adapter;
+interface Pagamento {
+    void processarPagamento(double valor);
+}
+
+class PagamentoBanco implements Pagamento {
+    @Override
+    public void processarPagamento(double valor) {
+        System.out.println("Pagamento de R$" + valor + " processado pelo banco.");
+    }
+}
+
+class PagamentoPayPal {
+    public void realizarTransacao(double quantia) {
+        System.out.println("Pagamento de R$" + quantia + " realizado via PayPal.");
+    }
+}
+
+class PagamentoPayPalAdapter implements Pagamento {
+    private PagamentoPayPal pagamentoPayPal;
+
+    public PagamentoPayPalAdapter(PagamentoPayPal pagamentoPayPal) {
+        this.pagamentoPayPal = pagamentoPayPal;
+    }
+
+    @Override
+    public void processarPagamento(double valor) {
+        pagamentoPayPal.realizarTransacao(valor);
+    }
 }
